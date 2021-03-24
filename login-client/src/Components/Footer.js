04e54d0826434a33cafd767085148549097afd3b
Anel Gabriel Dominguez <anelgabriel002@gmail.com>
2021-03-24 13:37:42 -0400

    Create Footer.js

---
+++ login-client/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Footer extends Component {
+    render(){
+        return(
+            <footer className="pie">By Anel Dominguez &copy; </footer>
+        );
+    }
+}
+
+
+
+export default Footer;
